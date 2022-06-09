2c68dd4cd0ba273be9b0302ef74a4c4273ec74fe
GoGioGo <go.gio.king.sc@gmail.com>
2022-06-08 22:48:10 -0400

    layers cargados desdel servidor

--- src/components/aumentar.tsx
+++ src/components/aumentar.tsx
@@ -1,6 +1,7 @@
 import { useAppUserDispatch } from '../store/hooks/appUserHook';
 import { useMapDispatch } from '../store/hooks/mapsHooks';
 import { useEffect } from 'react';
+import { SELECT_ALL_FILTERS } from '../constants/constants';
 
 function Aumentar() {
     let birthday = new Date(1995, 11, 17)
@@ -31,30 +32,45 @@ function Aumentar() {
     }
     const { replaceAppUser } = useAppUserDispatch();
 
-    let b = 'prueba';
-    let c = ['uno', 'dos'];
-
     const { getMapLayers } = useMapDispatch();
 
-    //const { detailed } = useMapState();
-    useEffect(() => {
+    const loadData = (tile: any, name?: string) => {
         let headers = new Headers();
         let token = 'GUEST';
         headers.append('Content-Type', 'application/json');
         headers.append('Authorization', 'Bearer ' + token);
-        fetch('https://confdevbc.mhfd.org/map', {
-            method: 'POST',
-            headers,
-            body: JSON.stringify({ table: "floodplains_non_fema" })
-        }).then(resp => resp.json())
-            .then(data => console.log(data))
-    }, []);
+        return new Promise((resolve) => {
+            (fetch('https://confdevbc.mhfd.org/map', {
+                method: 'POST',
+                headers,
+                body: JSON.stringify({ table: tile })
+            }).then(resp => resp.json())
+                .then(data => { return (data) }))
+                .then(tiles => {
+                    resolve(true);
+                    if (!name) getMapLayers(tile, tiles);
+                })
+        })
+    }
+
+    const changes = () => {
+        replaceAppUser(a);
+        const promesas: Promise<any>[] = [];
+        SELECT_ALL_FILTERS.forEach((layer) => {
+            if (typeof layer === 'object') {
+                layer.tiles.forEach((tiles: string) => {
+                    promesas.push(loadData(tiles, layer.name));
+                })
+            } else {
+                promesas.push(loadData(layer));
+            }
+        })
+    }
 
     return <>
         <div>
             <button onClick={() => {
-                replaceAppUser(a);
-                getMapLayers(b, c);
+                changes();
             }}>Aumentar</button>
         </div>
     </>
